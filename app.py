5ffd096e8776e05cd775763b1dd17ff572c65c2c
Linus Widing <s_widing21@stud.hwr-berlin.de>
2023-06-20 21:32:57 +0200

    Adding upload file functionality

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import io
 from threading import Thread
 
-from flask import Flask, session, render_template, redirect, url_for, request
+from flask import Flask, session, render_template, redirect, url_for, request, send_file
+
+import models
 from Server import server
 
 app = Flask(__name__)
@@ -31,6 +34,21 @@ def shell(shell_id):
             output = server.handle_command(cmd, client)
             client.append_output(output)
         return render_template("shell.html", client=client)
+
+
+@app.route("/shell/<shell_id>/upload_file", methods=["POST"])
+def test(shell_id):
+    shell_id = int(shell_id)
+    client = server.clients[shell_id]
+    if request.files:
+        if "file" in request.files:
+            file = request.files["file"]
+            in_mem_file = io.BytesIO()
+            file.save(in_mem_file)
+            print(in_mem_file.getbuffer().nbytes)
+            mem_file = models.MemFile(file.filename, in_mem_file)
+            client.add_file(mem_file)
+    return redirect(url_for("shell", shell_id=shell_id))
 
 
 if __name__ == '__main__':

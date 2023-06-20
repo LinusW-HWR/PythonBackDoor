32edc2f29d046c29d0e89e3285274ae8be344609
Linus Widing <s_widing21@stud.hwr-berlin.de>
2023-06-20 21:29:45 +0200

    Adding files to client class

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ class Client:
     addr = None
     socket = None
     output = []
+    files = []
 
     def __init__(self, pwd, addr, socket):
         self.pwd = pwd
@@ -13,6 +14,16 @@ class Client:
         cmd = str(cmd)
         for l in cmd.splitlines():
             self.output.append(l)
+
+    def add_file(self, file):
+        self.files.append(file)
+
+    def get_file_by_name(self, filename):
+        for f in self.files:
+            if f.name == filename:
+                return f
+        return None
+
 
 class MemFile:
     name = None
